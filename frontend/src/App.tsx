2a75a0753323baecc6f96f2d96402b1f7de7340a
Bharadwajreddy1406 <bharadwajreddy146@gmail.com>
2024-10-11 22:30:12 +0530

    done with controllers for context  routes in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,50 @@
-import { Routes ,Route} from 'react-router-dom'
-import './App.css'
-import { StudentInterviews } from './components/Student-Interviews'
-import { ProfileSection } from './components/Profile'
-import { NavbarComponent } from './components/navbar'
-import { InterviewDashboard } from './components/interview-dashboard'
-import { CompletedInterviews } from './components/completed-interviews'
-import { StudentPerformance } from './components/Student-Performance'
-import Studentlist from './components/Student-list'
-import  InterviewStudentList from './components/interview-student-list'
-import { AdminStudentDashboard } from './components/AdminStudentDashboard'
-import { LoginPageComponent } from './components/login-page'
-import {InterviewComponent} from './components/InterviewComponent'
-import axios from 'axios'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import './App.css';
+import { StudentInterviews } from './components/Student-Interviews';
+import { ProfileSection } from './components/Profile';
+import { NavbarComponent } from './components/navbar';
+import { InterviewDashboard } from './components/interview-dashboard';
+import { CompletedInterviews } from './components/completed-interviews';
+import { StudentPerformance } from './components/Student-Performance';
+import Studentlist from './components/Student-list';
+import InterviewStudentList from './components/interview-student-list';
+import { AdminStudentDashboard } from './components/AdminStudentDashboard';
+import { LoginPageComponent } from './components/login-page';
+import { InterviewComponent } from './components/InterviewComponent';
+import axios from 'axios';
+import { useAuth } from './Context/AuthContext';
 
-axios.defaults.baseURL = 'http://localhost:5000/api/v1'
-axios.defaults.withCredentials = true
-
+axios.defaults.baseURL = 'http://localhost:5000/api/v1';
+axios.defaults.withCredentials = true;
 
 function App() {
+  const auth = useAuth();
+  console.log(auth.isLoggedIn);
 
   return (
     <>
       <Routes>
         <Route element={<NavbarComponent />}>
-          <Route path='/' element={<InterviewDashboard />} />
-          <Route path='/profile' element={<ProfileSection />} />
-          <Route path='/interviews' element={<StudentInterviews/>} />
-          <Route path='/completed-interviews' element={<CompletedInterviews/>} />
-          <Route path='/student-performance' element={<StudentPerformance/>} />
-          <Route path='/student-list' element={<Studentlist/>} />
-          <Route path='/interview-student-list' element={<InterviewStudentList/>} />
-          <Route path='/students' element={<AdminStudentDashboard/>}/>
-          <Route path='login' element={<LoginPageComponent/>} />
-          <Route path='interview' element={<InterviewComponent/>} />
+          { !auth.isLoggedIn ? (
+            <Route path="*" element={<Navigate to="/login" />} />
+          ) : (
+            <>
+              <Route path="/" element={<InterviewDashboard />} />
+              <Route path="/profile" element={<ProfileSection />} />
+              <Route path="/interviews" element={<StudentInterviews />} />
+              <Route path="/completed-interviews" element={<CompletedInterviews />} />
+              <Route path="/student-performance" element={<StudentPerformance />} />
+              <Route path="/student-list" element={<Studentlist />} />
+              <Route path="/interview-student-list" element={<InterviewStudentList />} />
+              <Route path="/students" element={<AdminStudentDashboard />} />
+              <Route path="/interview" element={<InterviewComponent />} />
+            </>
+          )}
+          <Route path="/login" element={<LoginPageComponent />} />
         </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./Context/AuthContext";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </StrictMode>
+);
